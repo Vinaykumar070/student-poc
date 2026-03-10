5bffc0b83a0f308810e9c490f2c45e62111fac3d
Vinay <44109687+Vinaykumar070@users.noreply.github.com>
2026-03-10 17:06:51 +0530

    refresh

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-Test API
+// Test API
 app.get("/", (req, res) => {
     res.send("Student POC API Running");
 });
