58cc9f05f7ed42c1445f7dddbe9a7ff351ca5241
Vinay <44109687+Vinaykumar070@users.noreply.github.com>
2026-03-11 12:30:04 +0530

    student schema changed

--- models/Student.js
+++ models/Student.js
@@ -1,12 +1,41 @@
+// const mongoose = require("mongoose");
+
+// const studentSchema = new mongoose.Schema({
+//   studentId: String,
+//   name: String,
+//   email: String,
+//   course: String,
+//   age: Number,
+//   createdAt: Date
+// });
+
+// module.exports = mongoose.model("Student", studentSchema);
+
 const mongoose = require("mongoose");
 
 const studentSchema = new mongoose.Schema({
-  studentId: String,
-  name: String,
-  email: String,
-  course: String,
-  age: Number,
-  createdAt: Date
+  studentId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  course: {
+    type: String
+  },
+  age: {
+    type: Number
+  },
+  email: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model("Student", studentSchema);
